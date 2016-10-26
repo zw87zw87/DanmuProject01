ef2f392daf33967e339e79858242ecd4966c69bb
wei.zhang <wei.zhang@shuyun.com>
2016-10-26 19:43:28 +0800

    "11"

---
+++ src/main/java/com/danmu/app/DyBulletScreenApplication.java
@@ -0,0 +1,37 @@
+package com.danmu.app;
+
+import com.danmu.client.DyBulletScreenClient;
+import com.danmu.utils.KeepAlive;
+import com.danmu.utils.KeepGetMsg;
+
+/**
+ * @summary: 弹幕Demo程序启动类 
+ * @author: FerroD     
+ * @date:   2016-3-12   
+ * @version V1.0
+ */
+public class DyBulletScreenApplication
+{
+	//设置需要访问的房间ID信息
+//	private static final int roomId = 301712;
+	private static final int roomId = 58428;
+
+	//弹幕池分组号，海量模式使用-9999
+	private static final int groupId = -9999;
+	
+	public static void main(String[] args)
+	{
+		//初始化弹幕Client
+        DyBulletScreenClient danmuClient = DyBulletScreenClient.getInstance();
+        //设置需要连接和访问的房间ID，以及弹幕池分组号
+        danmuClient.init(roomId, groupId);
+        
+        //保持弹幕服务器心跳
+        KeepAlive keepAlive = new KeepAlive();
+        keepAlive.start();
+        
+        //获取弹幕服务器发送的所有信息
+        KeepGetMsg keepGetMsg = new KeepGetMsg();
+        keepGetMsg.start();
+	}
+}
