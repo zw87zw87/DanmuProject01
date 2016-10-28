339a4d94fbebce1cf9e5990f2fc877ccd6aec542
wei.zhang <wei.zhang@shuyun.com>
2016-10-28 17:15:43 +0800

    333

--- src/main/java/com/danmu/controller/DanmuController.java
+++ src/main/java/com/danmu/controller/DanmuController.java
@@ -2,17 +2,25 @@ package com.danmu.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("danmu")
+@RequestMapping("/danmu")
 public class DanmuController {
     private final static Logger logger = LoggerFactory.getLogger(DanmuController.class);
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @RequestMapping("/hello")
     public String danmuTest(){
+        String sql = "INSERT INTO chatmsg(rid,uid,nn,txt,level,col,ct) VALUES ('111','111','111','111','1','0','0');";
+        jdbcTemplate.execute(sql);
 
+        logger.info("1111111");
         return "hello";
     }
 }
--- web/WEB-INF/views/hello.jsp
+++ web/WEB-INF/views/hello.jsp
@@ -1,16 +1,16 @@
 <%--
   Created by IntelliJ IDEA.
   User: shuyun
-  Date: 2016/10/27
-  Time: 11:21
+  Date: 2016/10/28
+  Time: 15:24
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>index</title>
-  </head>
-  <body>
-  index
-  </body>
+<head>
+    <title>hello</title>
+</head>
+<body>
+11111111111
+</body>
 </html>
